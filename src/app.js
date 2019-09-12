9317862d41fee7dcc2c2faf82200cfb71e7e4a59
StickyFingies <sethtraman@gmail.com>
2019-09-12 18:36:33 -0500

    Database connections

--- src/app.js
+++ src/app.js
@@ -1,14 +1,75 @@
 "use strict";
 
 import express from "express";
-import path from "path";
+import mongodb from "mongodb";
 
 const app = express();
 
+let MongoClient = mongodb.MongoClient;
+const url = "mongodb+srv://admin:iygVjlrACQVOPTOs@cluster0-vc0nb.mongodb.net/test?retryWrites=true&w=majority";
+
+let concessionsDb = {};
+
+MongoClient.connect(url, (err, db) => {
+    if (err) throw err;
+
+    concessionsDb = db;
+});
+
 app.use(express.static("static"));
 
-app.get("/status", (req, res) => {
-	res.sendStatus(200);
+app.get("/menu", (req, res) => {
+    let dbo = concessionsDb.db("lths_concessions_app");
+
+    let foodQuery  = { type: 0 };
+    let drinkQuery = { type: 1 };
+    let candyQuery = { type: 2 };
+
+    let menuItems = {"food": [], "drinks": [], "candy": []};
+
+    dbo
+        .collection("menu_items")
+        .find(foodQuery)
+        .toArray((err, result) => {
+            if (err) throw err;
+
+            for (let item of result) {
+                menuItems.food.push({
+                    "name": item.name,
+                    "price": item.price
+                });
+            }
+
+            dbo
+                .collection("menu_items")
+                .find(drinkQuery)
+                .toArray((err, result) => {
+                    if (err) throw err;
+
+                    for (let item of result) {
+                        menuItems.drinks.push({
+                            "name": item.name,
+                            "price": item.price
+                        });
+                    }
+
+                    dbo
+                        .collection("menu_items")
+                        .find(candyQuery)
+                        .toArray((err, result) => {
+                            if (err) throw err;
+
+                            for (let item of result) {
+                                menuItems.candy.push({
+                                    "name": item.name,
+                                    "price": item.price
+                                });
+                            }
+
+                            res.json(menuItems);
+                        });
+                });
+        });
 });
 
 app.use((req, res, next) => {
--- static/index.js
+++ static/index.js
@@ -1,19 +1,59 @@
 let content  = document.getElementById("content");
 let menu     = document.getElementById("menu");
 let checkout = document.getElementById("checkout");
-let reciept  = document.getElementById("receipt");
+let reciept = document.getElementById("concessions-reciept");
+let recieptPrice = document.getElementById("reciept-price");
+
+let price = 0.00;
 
 checkout.onclick = () => {
-	menu.close();
-	ons.notification.toast("<i class='fas fa-check'></i>&nbsp;&nbsp;Order checked out!", {
-		timeout: 600
-	});
+    price = 0.00;
+    recieptPrice.innerText = "$" + price.toFixed(2).toString();
+
+    let recieptItems = document.getElementsByClassName("reciept-item");
+
+    while (recieptItems[0]) {
+        recieptItems[0].parentNode.removeChild(recieptItems[0]);
+    }
+
+    menu.close();
+    ons.notification.toast("<i class='fas fa-check'></i>&nbsp;&nbsp;Order checked out!", {
+        timeout: 600
+    });
 };
 
-let menuItems = document.querySelectorAll(".foo ons-list-item");
+const menuItemClickEvent = function (event) {
+    const itemName = this.children[1].innerText;
+    const itemPrice = this.children[2].innerText.split("$")[1];
 
-menuItems.forEach((item) => {
-	item.onclick = () => {
-		alert("bruh");
-	}
-});
+    price += Number(itemPrice);
+
+    recieptPrice.innerText = "$" + price.toFixed(2).toString();
+
+    let listItem = document.createElement("ons-list-item", {
+        //
+    });
+
+    listItem.className = "reciept-item";
+    listItem.innerText = itemName;
+    listItem.price = itemPrice;
+    listItem.onclick = recieptItemClickEvent;
+
+    reciept.children[2].appendChild(listItem);
+};
+
+const recieptItemClickEvent = function (event) {
+    this.parentNode.removeChild(this);
+    price -= this.price;
+    recieptPrice.innerText = "$" + price.toFixed(2).toString();
+}
+
+window.onload = () => {
+    setTimeout(() => {
+        let listItems = document.querySelectorAll("ons-list.concessions-menu ons-list-item");
+
+        for (let item of listItems) {
+            item.onclick = menuItemClickEvent;
+        }
+    }, 600);
+}
